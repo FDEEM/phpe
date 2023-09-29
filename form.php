c33cd95a9431531be3a3c6fd52f2cdf45b1d1085
Moksleivis <Moksleivis@kitm.local>
2023-09-29 11:28:03 +0300

    first commit

---
+++ form.php
@@ -0,0 +1,32 @@
+
+
+    <form action="" method="post">
+        <input type="text" placeholder="vardas" name="name">
+        <input type="text" placeholder="email" name="email">
+        <input type="password" placeholder="password" name="password">
+        <button type="submit"> Siusti</button>
+    </form>
+
+    <?php
+    if (isset($_GET['vardas'])) {
+        //$_GET will show up in the url
+        hello($_GET['vardas']);
+    }
+
+    if (isset($_POST['email'])) {
+        echo $_POST['name'];
+        echo '<br>';
+        echo $_POST['email'];
+        echo '<br>';
+
+        echo $_POST['password']; 
+    }
+
+
+    
+   # function hello($vardas) {
+    #    echo '<h1>Labas, ', $vardas ,'</h1>';
+    #}
+
+    
+    ?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charser="utf-8">
+        <meta name="viewport" content="widtn=divece-witdth, initial-scale">
+</head>
+<body>
+    <h1>testas</h1>
+
+
+    <?php
+        include 'form.php'
+    ?>
+
+</body>
+</html>
